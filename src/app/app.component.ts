5d044d4001e574738c6caf0069fc81560364fe37
Fishuke <gamecalp@gmail.com>
2020-12-07 20:19:19 +0100

    Add noinspection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'spoongular';
+  // noinspection SpellCheckingInspection
+  title = 'Spoongular';
 }
