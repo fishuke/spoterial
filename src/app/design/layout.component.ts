6013278371125278fd623c61cf34c85ecaa8f5cf
Fishuke <gamecalp@gmail.com>
2021-01-07 15:40:25 +0300

    Lint

--- src/app/design/layout.component.ts
+++ src/app/design/layout.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'sa-layout',
+  selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
