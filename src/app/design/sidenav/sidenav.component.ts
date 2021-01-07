cba964652c94c649ba2306254e161d521e80ae7c
Fishuke <gamecalp@gmail.com>
2021-01-07 15:42:48 +0300

    Lint

--- src/app/design/sidenav/sidenav.component.ts
+++ src/app/design/sidenav/sidenav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'sa-sidenav',
+  selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
