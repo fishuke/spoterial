8266ab474cba2240d2a89ff8ae70d644ad4b502e
Fishuke <gamecalp@gmail.com>
2021-01-07 15:40:06 +0300

    Lint

--- src/app/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard-page/dashboard-page.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'sa-dashboard-page',
+  selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
@@ -11,31 +11,31 @@ export class DashboardPageComponent implements OnInit {
   }
 
   songArray = [{
-    iconURL: "https://i.scdn.co/image/ab67616d0000b273d3004c7f9c57d391bbdb0850",
-    title: "Bangarang",
-    author: "skrillex"
+    iconURL: 'https://i.scdn.co/image/ab67616d0000b273d3004c7f9c57d391bbdb0850',
+    title: 'Bangarang',
+    author: 'skrillex'
   }, {
-    iconURL: "https://pbs.twimg.com/profile_images/962453106326827008/nYbSbw9N.jpg",
-    title: "Blood // Water",
-    author: "grandson"
+    iconURL: 'https://pbs.twimg.com/profile_images/962453106326827008/nYbSbw9N.jpg',
+    title: 'Blood // Water',
+    author: 'grandson'
   }, {
-    iconURL: "https://i.scdn.co/image/ab67616d0000b273d3004c7f9c57d391bbdb0850",
-    title: "First of the Year",
-    author: "skrillex"
+    iconURL: 'https://i.scdn.co/image/ab67616d0000b273d3004c7f9c57d391bbdb0850',
+    title: 'First of the Year',
+    author: 'skrillex'
   },
   {
-    iconURL: "https://pbs.twimg.com/profile_images/962453106326827008/nYbSbw9N.jpg",
-    title: "Rock Bottom",
-    author: "grandson"
+    iconURL: 'https://pbs.twimg.com/profile_images/962453106326827008/nYbSbw9N.jpg',
+    title: 'Rock Bottom',
+    author: 'grandson'
   },
   {
-    iconURL: "https://i.scdn.co/image/ab67616d0000b273d3004c7f9c57d391bbdb0850",
-    title: "Kyoto",
-    author: "skrillex"
+    iconURL: 'https://i.scdn.co/image/ab67616d0000b273d3004c7f9c57d391bbdb0850',
+    title: 'Kyoto',
+    author: 'skrillex'
   }, {
-    iconURL: "https://pbs.twimg.com/profile_images/962453106326827008/nYbSbw9N.jpg",
-    title: "Overdose",
-    author: "grandson"
+    iconURL: 'https://pbs.twimg.com/profile_images/962453106326827008/nYbSbw9N.jpg',
+    title: 'Overdose',
+    author: 'grandson'
   }
   ];
 
