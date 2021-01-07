0aec21e5484058934c899b849f35a425a9bd081e
Fishuke <gamecalp@gmail.com>
2021-01-07 15:39:52 +0300

    Remove unused imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {AppComponent} from './app.component';
-import {NotFoundPageComponent} from './pages/not-found-page/not-found-page.component';
 import {LayoutComponent} from './design/layout.component';
 import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
 
